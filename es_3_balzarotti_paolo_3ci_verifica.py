8b7bc1d31a40f99c7f5717b1f19ea3d3f4e023bd
balza08 <balzap08@gmail.com>
2024-11-19 13:05:16 +0100

    ve
    ve

---
+++ es_3_balzarotti_paolo_3ci_verifica.py
@@ -0,0 +1,8 @@
+print("in caso di lancio nullo inserisci 0: ")
+lancio1=int(input("Inserisci la lunghezza del primo lancio: "))
+lancio2=int(input("Inserisci la lunghezza del secondo lancio: "))
+lancio3=int(input("Inserisci la lunghezza del terzo lancio: "))
+lancio4=int(input("Inserisci la lunghezza del quarto lancio: "))
+lancio5=int(input("Inserisci la lunghezza del quinto lancio: "))
+medial=(lancio1+lancio2+lancio3+lancio4+lancio5)/5
+print(f"La media dei lanci è: {medial}")
